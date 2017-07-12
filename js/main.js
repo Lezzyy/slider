d4caeda734141e50e84471ac39f31e52a9087735
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-12 16:19:51 +0200

    slidshow ok

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
-var slide = document.getElementsByClassName('slide');
-var previous = document.getElementsByClassName('previous');
-var next = document.getElementsByClassName('next');
-slider();
+var slide = document.getElementsByClassName("slide");
+//var allSlides = document.getElementsByClassName('allSlides');
+//var previous = document.getElementsByClassName('previous');
+//var next = document.getElementsByClassName('next');
+var interval =  setInterval(carousel, 2000);
+var base=0;
 
-
-
-function slider(){
-var slideChange = 0;
+function carousel(){
 for (var i=0; i<slide.length; i++){
   slide[i].style.display = 'none';
+  }
+  base++;
+    if (base>slide.length){
+      base=1;
+  }
+  slide[base-1].style.display='block';
 }
-slideChange++;
-if (slideChange>slide.length) {
-  slideChange=1;
-}
-slide[slideChange-1].style.display='block';
-setInterval(slider, 2000);
-}
-/*setInterval(function(){
-  slide[i];}, 3000);*/
+
+carousel();
